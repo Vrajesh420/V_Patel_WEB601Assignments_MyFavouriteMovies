c47097c85312236a16b465f8c0ac9cad2d2bc95a
vrajesh420 <125092320+Vrajesh420@users.noreply.github.com>
2024-02-07 18:34:56 -0500

    Assignment 3 done

--- V_Patel_MyFavouriteMovies/src/app/content-list/content-list.component.ts
+++ V_Patel_MyFavouriteMovies/src/app/content-list/content-list.component.ts
@@ -1,19 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
+import { ContentCardComponent } from '../content-card/content-card.component';
+import { TypereactPipe } from '../typereact.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ContentCardComponent, TypereactPipe, FormsModule],
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.scss'
 })
 export class ContentListComponent implements OnInit {
-  displayContentInfo(contentItem: Content) {
+  DisplayContentInformation(contentItem: Content) {
     console.log(`ID: ${contentItem.id} and Title: ${contentItem.title}`);
     }
   @Input () contentItems: Content[] = [];
+
+  searchTitle: string = '';
+  contentExists: boolean = false;
+  message: string = '';  
+  selectedTitle: string | null = null;
+
+  checkContentExists() {
+    const foundItem = this.contentItems.find(item => item.title.toLowerCase() === this.searchTitle.toLowerCase());
+    this.contentExists = !!foundItem;
+    this.message = foundItem ? 'Content item exists.' : 'Content item does not exist.';
+    this.selectedTitle = foundItem ? foundItem.title : null;
+  }
+
 
   ngOnInit(): void {
     this.contentItems =[
@@ -23,7 +39,7 @@ export class ContentListComponent implements OnInit {
         description:"A movie of mystery hidden in music equipment.",
         creator:"Rohit Shetty",
         imgURL:"http://4.bp.blogspot.com/-EZW9AbiHYv8/TcU6TMpUexI/AAAAAAAABfk/_x4IXylyKuI/s1600/still11.jpg",
-        type:"Comedy",
+        type:"",
         tags: ["Funny", "Superstars"]
       },
       {
@@ -32,7 +48,7 @@ export class ContentListComponent implements OnInit {
         description:"The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb.",
         creator:"Christopher Nolar",
         imgURL:"https://movies.universalpictures.com/media/03-opp-dm-mobile-banner-1080x745-pl-f01-062122-62defaa505f81-1.jpg",
-        type:"Drama",
+        type:"Fictional",
         tags: ["Nuclear", "Real"]
       },
       {
@@ -41,7 +57,7 @@ export class ContentListComponent implements OnInit {
         description:"Historical movie with star actors.",
         creator:"Ramesh Sippy",
         imgURL:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhZu5Uyspc49EPwn1VSpkbJA7OgS3oXwkZ6eLqnZLM5oqsRfrExikbmQHtSP2HidV9n_gPPZZghrhouagxQIuAUOEHYDWtUdzzoffpFIsrCnpK_BcG2-UVvDD-LE7yAWFq0MUtK9owkWsctXTnXs-hhKa18RPA_pEOkanZMlN7AjZaIjR4pcYbZIGNg/s1280/Sholey%20Movie.jpg",
-        type:"Thriller",
+        type:"Fictional",
         tags: ["Old", "History"]
       },
       {
@@ -50,7 +66,7 @@ export class ContentListComponent implements OnInit {
         description: "A movie of hilarious robbery.",
         creator: "Rohit Shetty",
         imgURL: "https://s-media-cache-ak0.pinimg.com/originals/e7/e9/b4/e7e9b41eb807b9a01fe35ce27cd4f326.jpg",
-        type: "Action",
+        type: "Non-Fictional",
         tags: ["Suspense", "Action"]
       },
       {
@@ -59,8 +75,26 @@ export class ContentListComponent implements OnInit {
         description: "Movie connected to indian super hero.",
         creator: "Abhishek",
         imgURL: "https://jiocinemaweb.cdn.jio.com/jioimages.cdn.jio.com/content/entry/dynamiccontent/thumbs/1680/-/0/96/98/d70504c0744311e689eddbce9487c672_1605257202446_l_medium.jpg",
-        type: "Fictional",
+        type: "Non-Fictional",
         tags: ["Heroes", "Mystrey"]
+      },
+      {
+        id: 5,
+        title: "URI",
+        description: "Movie about india army.",
+        creator: "Harsh",
+        imgURL: "https://m.media-amazon.com/images/M/MV5BMWU4ZjNlNTQtOGE2MS00NDI0LWFlYjMtMmY3ZWVkMjJkNGRmXkEyXkFqcGdeQXVyNjE1OTQ0NjA@._V1_FMjpg_UX1000_.jpg",
+        type: "Fictional",
+        tags: ["Action", "Border"]
+      },
+      {
+        id: 6,
+        title: "Agent vinod",
+        description: "Movie about indian spy.",
+        creator: "Saif",
+        imgURL: "https://upload.wikimedia.org/wikipedia/en/5/5e/Agent_Vinod_2.jpg",
+        type: "Non-Fictional",
+        tags: ["Spy", "Mystrey"]
       }
       
     ];
---
+++ V_Patel_MyFavouriteMovies/src/app/typereact.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TypereactPipe } from './typereact.pipe';
+
+describe('TypereactPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TypereactPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
